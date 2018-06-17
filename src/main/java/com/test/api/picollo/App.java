73c51f89515075d36c548cd7215135ee64951263
anx-manxy <harvy.torres@asiainspection.com>
2018-06-17 15:59:09 +0800

    Added log4j2

--- src/main/java/com/test/api/picollo/App.java
+++ src/main/java/com/test/api/picollo/App.java
@@ -1,13 +1,17 @@
 package com.test.api.picollo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.info( "Hello World!" );
     }
 }
